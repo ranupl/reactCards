96e068052a00ffcd1b7d5ced23b6cf0c05467fe0
ranupl <ranupl542011@gmail.com>
2024-01-02 15:59:35 +0530

    react step processs

---
+++ cards/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "./components/Card";
+import data from "../src/data.json";
+
+function App() {
+  const length = data.length;
+
+  return (
+    <div>
+      <div className="container">
+        <div className="heading">
+          <h1>
+            Our Process After You <span>Depart</span>
+          </h1>
+        </div>
+        <div className="showCards">
+          <div className="cards">
+            {data.map((item, index) => {
+              return (
+                <Card key={index} data={item} index={index} len={length} />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ cards/src/components/Card.js
@@ -0,0 +1,69 @@
+import React from "react";
+import women from "../images/women.jpg";
+import { TbTriangleFilled } from "react-icons/tb";
+import { TbTriangleInvertedFilled } from "react-icons/tb";
+import { TbTriangleInverted } from "react-icons/tb";
+import { FaRegCircle } from "react-icons/fa";
+
+const Card = ({ data, index, length }) => {
+  return (
+    <div>
+      {index % 2 == 0 ? (
+        <div className="cardContainer">
+          <div className="innerContainer">
+            <div className="box">
+              <h5>{data.heading}</h5>
+              <p>{data.paragraph}</p>
+            </div>
+          </div>
+          <div className="iconsContainer">
+            <span className="icon1">
+              {/* <FaRegCircle /> */}
+              <TbTriangleInvertedFilled />
+            </span>
+            <span className="icon2">
+              {/* <TbTriangleInverted />  */}
+              <TbTriangleFilled />
+            </span>
+          </div>
+          <div className="imgContainer">
+            <img
+              src={data.imageURL}
+              alt=""
+              width="170px"
+              height="100px"
+              className="img-org"
+            />
+            <div className="indexContainer">
+              <span className="index">{index + 1}</span>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="cardContainer">
+          <div className="imgContainer-rev">
+            <img
+              src={data.imageURL}
+              alt=""
+              width="170px"
+              height="100px"
+              className="rev-img"
+            />
+            <div className="indexContainer">
+              <span className="index-rev">{index + 1}</span>
+            </div>
+          </div>
+
+          <div className="innerContainer">
+            <div className="box-rev">
+              <h5>{data.heading}</h5>
+              <p>{data.paragraph}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Card;
